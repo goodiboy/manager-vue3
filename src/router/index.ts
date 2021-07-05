00610229345d3d84becd33f936c358cacb94773d
阮传宙 <ruanchuanzhou@interlib.vip>
2021-07-05 23:54:44 +0800

    完成路由、jsx配置

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import {createRouter, createMemoryHistory, RouteRecordRaw} from "vue-router";
+import Home from "../components/Home";
+const routes: Array<RouteRecordRaw> = [
+  {
+    name: 'Home',
+    path: '/',
+    component: Home
+  }
+]
+
+
+const router =  createRouter({
+  history: createMemoryHistory(''),
+  routes
+})
+
+export default  router
