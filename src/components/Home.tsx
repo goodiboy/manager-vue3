6fda4e05511a4a595e47fcc3864a620c2f9427cb
阮传宙 <ruanchuanzhou@interlib.vip>
2021-09-06 23:52:03 +0800

    fix: home文件使用module模块样式

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,19 @@
 import { defineComponent } from 'vue'
-import './home.scss'
+import home from './home.module.scss'
 export default defineComponent({
   name: 'Home',
   setup() {
     return () => (
-      <div id="home-module-css-id">
-        <div class="basic-layout">
-          <div class="nav-side" />
-          <div class="content-right">
-            <div class="nav-top">
-              <div class="bread">面包屑</div>
-              <div class="user-info">用户</div>
+      <div class={home.home}>
+        <div class={home['basic-layout']}>
+          <div class={home['nav-side']} />
+          <div class={home['content-right']}>
+            <div class={home['nav-top']}>
+              <div>面包屑</div>
+              <div>用户</div>
             </div>
-            <div class="wrapper">
-              <div class="main-page">
+            <div class={home.wrapper}>
+              <div class={home['main-page']}>
                 <router-view />
               </div>
             </div>
