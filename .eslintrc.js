e4df317119480c1683266d30f1e09c85cc2d564b
阮传宙 <ruanchuanzhou@interlib.vip>
2021-09-10 16:21:30 +0800

    config: 调整eslint的essential为vue3版本

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,13 @@ module.exports = {
     node: true
   },
   extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
-    'plugin:vue/essential',
     'prettier'
   ],
   rules: {
